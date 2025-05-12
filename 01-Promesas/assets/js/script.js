3b42cd93c3aa74d2354b1b7ad9c79114be0da2b1
Jacobo valencia <jacoboval@gmail.com>
2025-05-12 16:17:25 -0600

    implementa Try/Catch

--- 01-Promesas/assets/js/script.js
+++ 01-Promesas/assets/js/script.js
@@ -4,23 +4,30 @@ async function getDatosClima(){
     
     const url = `https://api.open-meteo.com/v1/forecast?latitude=${lat.value}&longitude=${lon.value}&current=temperature_2m,relative_humidity_2m,rain,wind_speed_10m&timezone=auto`;
 
-    const response = await fetch(url);
-    if (!response.ok) {
-        console.log("Error al resolver la promesa");
-        return;
-    }
-    const data = await response.json();
-    const weatherData = document.getElementById('weatherData');
+    try{
+        const response = await fetch(url);
+        if (!response.ok) {
+            console.log("Error al resolver la promesa");
+            return;
+        }
+        const data = await response.json();
+        const weatherData = document.getElementById('weatherData');
 
-    const temperatureElement = document.getElementById('temperature');
-    const windspeedElement = document.getElementById('windspeed');
-    const humedadElement = document.getElementById("humidity");
-    const rainElement = document.getElementById("rain");
+        const temperatureElement = document.getElementById('temperature');
+        const windspeedElement = document.getElementById('windspeed');
+        const humedadElement = document.getElementById("humidity");
+        const rainElement = document.getElementById("rain");
 
-    weatherData.classList.remove('hidden');
-    temperatureElement.textContent = `${data.current.temperature_2m} °C`;
-    windspeedElement.textContent = `${data.current.wind_speed_10m} Km/h`;
-    humedadElement.textContent = `${data.current.relative_humidity_2m} %`;
-    rainElement.textContent = `${data.current.rain}mm`;
+        weatherData.classList.remove('hidden');
+        temperatureElement.textContent = `${data.current.temperature_2m} °C`;
+        windspeedElement.textContent = `${data.current.wind_speed_10m} Km/h`;
+        humedadElement.textContent = `${data.current.relative_humidity_2m} %`;
+        rainElement.textContent = `${data.current.rain}mm`;
+    
+    }catch(error) {
+    console.log(error)
+    alert(error);
+
+}
   
 }

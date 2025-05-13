b01ae5b73440dc2df351713050d8d9e26737ccca
Jacobo valencia <jacoboval@gmail.com>
2025-05-12 18:17:40 -0600

    promesas en simultaneo

--- 01-Promesas/assets/js/script.js
+++ 01-Promesas/assets/js/script.js
@@ -1,4 +1,15 @@
-async function getDatosClima() {
+async function getDatos() {
+     const [dataActual,dataFuura] = await Promise.all(
+        [
+            getDatosClimaActual(),
+            getDatosClimaFuturo()
+        ]
+    );
+    presentaDatosActuales(dataActual);
+    presentaDatosFuturos(dataFuura);
+    
+}
+async function getDatosClimaActual() {
     const lat = document.getElementById('latitude');
     const lon = document.getElementById('longitude');
 
@@ -10,24 +21,9 @@ async function getDatosClima() {
             throw new Error("Error al resolver la promesa");
         }
         const data = await response.json();
-        const weatherData = document.getElementById('weatherData');
-
-        const temperatureElement = document.getElementById('temperature');
-        const windspeedElement = document.getElementById('windspeed');
-        const humedadElement = document.getElementById("humidity");
-        const rainElement = document.getElementById("rain");
-
-        weatherData.classList.remove('hidden');
-        temperatureElement.textContent = `${data.current.temperature_2m} °C`;
-        windspeedElement.textContent = `${data.current.wind_speed_10m} Km/h`;
-        humedadElement.textContent = `${data.current.relative_humidity_2m} %`;
-        rainElement.textContent = `${data.current.rain}mm`;
-
-        await getDatosClimaFuturo();
-
+        return data.current;
     } catch (error) {
-        console.log(error)
-        alert(error);
+        throw error;
     }
 }
 
@@ -37,21 +33,16 @@ async function getDatosClimaFuturo() {
 
     //  tiempo a futuro por 7 dias, proporcionados por la API 
     const url = `https://api.open-meteo.com/v1/forecast?latitude=${lat.value}&longitude=${lon.value}&daily=temperature_2m_max,temperature_2m_min,precipitation_sum&timezone=auto`;
-    const forecastData = document.getElementById('forecastData');
-
+ 
     try {
         const response = await fetch(url);
         if (!response.ok) {
             throw new Error("Error al resolver la promesa");
         }
         const data = await response.json();
-        presentaDatosFuturos(data.daily);
-        forecastData.classList.remove('hidden');
-
-
-
+        return data.daily;
     } catch (error) {
-        console.log(error);
+        throw error;
     }
 
 }
@@ -60,30 +51,46 @@ función llamada formatDate, que convierte la fecha al idioma deseado, ya que or
 está en formato anglosajón o en segundos desde 1970. Usaremos toLocaleDateString para 
 obtener la fecha en español, ya sea de España, México o Argentina.
 */
+function presentaDatosActuales(currentWheater){
+    const weatherData = document.getElementById('weatherData');
+
+    const temperatureElement = document.getElementById('temperature');
+    const windspeedElement = document.getElementById('windspeed');
+    const humedadElement = document.getElementById("humidity");
+    const rainElement = document.getElementById("rain");
+
+    weatherData.classList.remove('hidden');
+    temperatureElement.textContent = `${currentWheater.temperature_2m} °C`;
+    windspeedElement.textContent = `${currentWheater.wind_speed_10m} Km/h`;
+    humedadElement.textContent = `${currentWheater.relative_humidity_2m} %`;
+    rainElement.textContent = `${currentWheater.rain}mm`;    
+}
 
 function formatDate(dateString){
     const date = new Date(dateString);
     return date.toLocaleDateString('es-Es', {weekday: 'short',month:'short',day:'numeric'});
     
 }
-function presentaDatosFuturos(forecastData){
+function presentaDatosFuturos(forecasWheatertData){
+    const forecastData = document.getElementById('forecastData');       
     const forecastGrid = document.getElementById('forecastGrid')
     forecastGrid.innerHTML = '';
 
-    for (let i = 0;i<  forecastData.time.length; i++){
+    for (let i = 0; i < forecasWheatertData.time.length; i++){
         const forecastItem = document.createElement('div');
         forecastItem.className = "forecast-item"
+        
         forecastItem.innerHTML =`
-            <div class="forecast-date">${formatDate(forecastData.time[i])}</div>
+            <div class="forecast-date">${formatDate(forecasWheatertData.time[i])}</div>
             <div class="forecast-temp">
-            <span class="forecast-temp-max">${Math.round(forecastData.temperature_2m_max[i])}°</span>
-            <span class="forecast-temp-min">${Math.round(forecastData.temperature_2m_min[i])}°</span>
+            <span class="forecast-temp-max">${Math.round(forecasWheatertData.temperature_2m_max[i])}°</span>
+            <span class="forecast-temp-min">${Math.round(forecasWheatertData.temperature_2m_min[i])}°</span>
             </div>
             <div class="forecast-precip">
-            Lluvia: ${forecastData.precipitation_sum[i]} mm
+            Lluvia: ${forecasWheatertData.precipitation_sum[i]} mm
             </div>       
         `;
-        forecastGrid.appendChild(forecastItem);
-        forecastGrid.classList.remove('hidden');        
+        forecastGrid.appendChild(forecastItem);                  
+        forecastData.classList.remove('hidden');
     }
 }

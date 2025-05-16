b8f0812341af165e528f5a53045c73b8a8672181
Jacobo valencia <jacoboval@gmail.com>
2025-05-16 10:04:25 -0600

    mostrando repositorios, planos

--- 02-ApiGitHub/js/script.js
+++ 02-ApiGitHub/js/script.js
@@ -1,4 +1,6 @@
 const searchInput = document.querySelector('#searchInput');
+const errorMessage = document.getElementById('errorMessage');
+const loading = document.getElementById('loading');
 searchInput.addEventListener('keypress',(event) => {
     if (event.key === 'Enter'){
         buscarRepositorios();
@@ -7,13 +9,51 @@ searchInput.addEventListener('keypress',(event) => {
 
 async function buscarRepositorios() {
     const endPoint = `https://api.github.com/users/${searchInput.value}/repos`;
+    ocultarError();
+    mostrarCargando();
     //alert(endPoint);
 
     //const response = await fetch(endPoint);
     const response = await fetch(endPoint, {method:'GET'});
-    const data = await response.json();
-
-    console.log(data);
     
+    
+    if(!response.ok){
+        mostrarError("No fue posible consultarlso repositorios de ese usuario.");
+
+    }
+    const data = await response.json();
+    //console.log(data);
+    ocultarCargando();
+    mostarRepositorios(data);
+}
+function mostarRepositorios(repos){
+    const reposContainer = document.getElementById('reposContainer');
+    if(repos.length === 0){
+        mostrarError=("El usuario no tiene repositorios.");
+    }
+    reposContainer.innerHTML = '';
+
+    repos.forEach(repo => {
+        reposContainer.innerHTML += `<div>${repo.name}</div>`;
+       
+
+    });   
+}
+
+function mostrarError(mensaje){
+    //const errorMessage = document.getElementById('errorMessage');
+    errorMessage.textContent = mensaje;
+    errorMessage.style.display = 'block';
+} 
+function ocultarError(){
+    errorMessage.style.display = '';
+}
+
+function mostrarCargando(){
+    loading.style.display='block';
+}
+
+function ocultarCargando() {
+    loading.style.display='';
 
 }
